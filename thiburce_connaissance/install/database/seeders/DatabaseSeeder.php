e5b07e3af5817299f38020d5a3be811e66a5fd26
Wariss Achamou Adeniyi BADA <wariss.bada@chem.loc>
2025-11-21 17:33:33 +0100

    feat:auth and roles setup , install Laravel Breeze  , Add avatar column to users table ,  Install Spatie permission, package, config user model with hasroles, seed datauser

--- thiburce_connaissance/install/database/seeders/DatabaseSeeder.php
+++ thiburce_connaissance/install/database/seeders/DatabaseSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Role;
 
 class DatabaseSeeder extends Seeder
 {
@@ -15,11 +16,38 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
+        // 1.creation des roles
+        $adminRole= Role::create(attributes:['name' => 'admin']);
+        $userRole= Role::create(attributes:['name' => 'user']);
+
+
+        //2.cree de ladministrateur
+        $admin =  User::factory()->create(attributes:[
+            'name' => 'Admin User',
+            'email'=> 'admin@example.com',
+            'password'=> bcrypt('password'),
+        ]);
+
+        //attribution du role admin au user admin
+        $admin->assignRole($adminRole);
+            
+        //3. d'un user standar pour tester
+
+        $user = User::factory()->create(attributes:[
+            'name' => 'Alice  User',
+            'email'=> 'user@higfive.com',
+            'password'=> bcrypt('password'),
+        ]);
+
+        //attribution du role user au user alice ou utilisateur
+        $user->assignRole($userRole);
+
+
         // User::factory(10)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+    //     User::factory()->create([
+    //         'name' => 'Test User',
+    //         'email' => 'test@example.com',
+    //     ]);
     }
 }
